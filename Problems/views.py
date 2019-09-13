d4609e1c6f59969512a54cd9b11428d9c4588c64
rishabhgaurav007 <rishabhgaurav007@gmail.com>
2019-09-13 13:33:47 +0530

    Updated Submit and compile feature

--- Problems/views.py
+++ Problems/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from Problems.models import ProblemSet
+from Problems import littlechecker as lc
 import subprocess
+from .forms import SubmissionForm
 
 # Create your views here.
 def problemIndex(request):
@@ -24,17 +26,28 @@ def problemIndex(request):
 def problemWithID(request,problemID):
     try:
         problem = ProblemSet.objects.get(pk = problemID)
-        return render(request, 'problemPage.html', {'problem': problem})
+        form =SubmissionForm()
+        return render(request, 'problemPage.html', {'problem': problem,'form':form})
     except:
         return render(request, 'index.html')
 
 def problemSubmit(request,problemID):
     language,codeFile = None,None
+    print("$")
     if request.method == "POST":
-        language = request.POST.get("language",None)
-        codeFile = request.POST.get("codeFile",None)
+        form = SubmissionForm(request.POST, request.FILES)
+        print("%")
+        if form.is_valid():
+            print("^")
+            language = request.POST.get("language",None)
+            for fields in form:
+                codeFile=fields
     if language == None or codeFile == None:
+        print("))")
         return render(request,'submit.html',{'problemID': problemID, 'returnStatus': 1})
     else:
-        x = 0
+        print(lc.compile('tests.c',language))
         #write code for system check
+        return render(request,'submit.html',{'problemID': problemID, 'returnStatus': 1})
+
+
